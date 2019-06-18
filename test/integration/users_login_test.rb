9caeb78c65cdabd6a64213d3decd613e034c48b7
greybutton <greybutton@yandex.com>
2019-06-18 13:35:38 +0800

    10.3.1 users index

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,6 +26,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', users_path
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -35,6 +37,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
     assert_select 'a[href=?]', user_path(@user), count: 0
+    assert_select 'a[href=?]', edit_user_path(@user), count: 0
+    assert_select 'a[href=?]', users_path, count: 0
   end
 
   test 'login with remembering' do
